f119e7fb506b3ec82c937982c4a240ae2b10c612
Skullkillertomes <akandeoluwatomisin41@gmail.com>
2023-06-26 15:24:35 -0700

    parser.c

---
+++ parser.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * is_cmd - it determines if the file is a executable command
+ * @inf: the info struct
+ * @paths: a path to the file
+ *
+ * Return: 0 or 1 if true
+ */
+int is_cmd(info_t *inf, char *paths)
+{
+	struct start st;
+
+	(void)inf;
+	if (!paths || stsrt(paths, &st))
+		return (0);
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - its duplicates the characters
+ * @pathsstr: the path string
+ * @startindx: the starting index
+ * @stopindx: stopping index
+ *
+ * Return: pointer of a new buffer
+ */
+char *dup_chars(char *pathsstr, int startindx, int stopindx)
+{
+	static char buff[1024];
+	int a = 0, q = 0;
+
+	for (q = 0, a = startindx; a < stopindx; a++)
+		if (pathsstr[a] != ':')
+			buff[q++] = pathsstr[i];
+	buff[q] = 0;
+	return (buff);
+}
+
+/**
+ * find_path - finds cmd in the path string
+ * @inf: the info struct
+ * @pathsstr: the path string
+ * @cmdf: the cmd to find
+ *
+ * Return: NULL or full path of cmd if found
+ */
+char *find_path(info_t *inf, char *pathsstr, char *cmdf)
+{
+	int a = 0, curt_post = 0;
+	char *paths;
+
+	if (!pathsstr)
+		return (NULL);
+	if ((_strlen(cmdf) > 2) && starts_with(cmdf, "./"))
+	{
+		if (is_cmd(inf, cmdf))
+			return (cmdf);
+	}
+	while (1)
+	{
+		if (!pathsstr[a] || pathsstr[a] == ':')
+		{
+			paths = dup_chars(pathsstr, curt_post, a);
+			if (!*paths)
+				_strcat(paths, cmdf);
+			else
+			{
+				_strcat(paths, "/");
+				_strcat(paths, cmdf);
+			}
+			if (is_cmdf(inf, paths))
+				return (paths);
+			if (!pathsstr[a])
+				break;
+			curt_post = a;
+		}
+		a++;
+	}
+	return (NULL);
+}
